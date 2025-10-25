966a5914dcb2c037ba11cb3c592071b1aae1db6f
grozaqueen <yuliyavolkova12@yandex.ru>
2025-10-25 14:32:31 +0300

    исправлены импорты

--- app/generator_service/main.py
+++ app/generator_service/main.py
@@ -1,7 +1,7 @@
 import sys
-from service.context_builder import build_context_for_query
-from service.prompt_templates import make_prompt
-from service.generator import generate
+from .service.context_builder import build_context_for_query
+from .service.prompt_templates import make_prompt
+from .service.generator import generate
 
 def generate_post(user_query: str) -> str:
     ctx = build_context_for_query(user_query)
@@ -11,5 +11,5 @@ def generate_post(user_query: str) -> str:
     return generate(messages)
 
 if __name__ == "__main__":
-    q = " ".join(sys.argv[1:]) or  "Напиши пост про популярную породу кошек"
+    q = " ".join(sys.argv[1:]) or "Напиши пост про популярную породу кошек"
     print(generate_post(q))
